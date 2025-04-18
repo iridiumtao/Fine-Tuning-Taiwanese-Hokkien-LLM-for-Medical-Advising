c1834521e362b0eff751657b8dc0cc7cad3fcf00
Ping-Jung Lu <ping-junglu@macbookair.mynetworksettings.com>
2025-04-17 23:19:47 -0400

    prompt

--- inference.py
+++ inference.py
@@ -17,6 +17,7 @@ question = input("請輸入你的問題：")
 prompt = f"<|user|>\n{question}\n<|assistant|>\n"
 
 
+
 # Tokenize
 inputs = tokenizer(prompt, return_tensors = "pt").to(device)
 
@@ -28,7 +29,8 @@ with torch.no_grad():
         do_sample = True,
         top_p = 0.95,
         temperature = 0.7,
-        pad_token_id = tokenizer.eos_token_id
+        pad_token_id = tokenizer.eos_token_id,
+        eos_token_id = tokenizer.eos_token_id
     )
 
 # Decode
