ea7ac56d07210c77d72a9a2e00d27bd48547e950
yc7690 <yc7690@nyu.edu>
2025-05-02 17:37:56 -0400

    edit for floating ip

--- iac/tf/chi/outputs.tf
+++ iac/tf/chi/outputs.tf
@@ -3,3 +3,6 @@ output "floating_ip_out" {
   value       = openstack_networking_floatingip_v2.floating_ip.address
 }
 
+output "sharednet2_port_id" {
+  value = openstack_networking_port_v2.sharednet2_port.id
+}
